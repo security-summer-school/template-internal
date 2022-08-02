4719fb9b1ca7164c910b3db3a757ca046d426c14
Gabriel Mocanu <gabi.mocanu98@gmail.com>
2022-08-03 01:19:30 +0300

    Replace := with ?= and add new line to solution.py script
    Signed-off-by: Gabriel Mocanu <gabi.mocanu98@gmail.com>

--- common/challenge.mk
+++ common/challenge.mk
@@ -1,7 +1,7 @@
-INTERNAL_PORT := 80
-CONT_NAME := ${IMG_NAME}
-FILE_FLAG := ../flag
-FLAG := $(shell cat $(FILE_FLAG))
+INTERNAL_PORT ?= 80
+CONT_NAME ?= ${IMG_NAME}
+FILE_FLAG ?= ../flag
+FLAG ?= $(shell cat $(FILE_FLAG))
 
 run: generate build
 	docker run -d -p $(EXTERNAL_PORT):$(INTERNAL_PORT) --name $(CONT_NAME) -t $(IMG_NAME)
