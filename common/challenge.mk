393a6aef8c8c7b9312888b126cab0351cb98a5f3
Gabriel Mocanu <gabi.mocanu98@gmail.com>
2022-08-03 01:59:56 +0300

    Update common Makefile to use also the Makefile from /src if exists
    Signed-off-by: Gabriel Mocanu <gabi.mocanu98@gmail.com>

--- common/challenge.mk
+++ common/challenge.mk
@@ -10,12 +10,14 @@ build: generate
 	docker build -t $(IMG_NAME) -f Dockerfile ..
 
 generate:
+	[ -f ../src/Makefile ] && make -C ../src/	|| echo 'No Makefile detected'
 	sed 's/__TEMPLATE__/$(FLAG)/g' $(FILE_TEMPLATE) > $(FILE_SRC)
 
 stop:
 	docker stop $(CONT_NAME)
 
 clean: stop
+	[ -f ../src/Makefile ] && make -C ../src/ clean || echo 'No Makefile detected'
 	docker rm $(IMG_NAME)
 	docker image rm $(IMG_NAME):latest
 	rm -f $(FILE_SRC)
